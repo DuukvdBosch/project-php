d5b979c47b2e0cc4cceb12f7021caaf13f8b9e23
DuukvdBosch <duukvandenbosch@gmail.com>
2023-06-06 12:41:42 +0200

    jimdrainer

--- site/index.php
+++ site/index.php
@@ -1,5 +1,52 @@
-<?php
-
-$jim = "Bart Simpson";
-
-echo $jim;
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Responsive Grid Homepage</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<header>
+		<h1>Responsive Grid Homepage</h1>
+		<nav>
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">About</a></li>
+				<li><a href="#">Contact</a></li>
+			</ul>
+		</nav>
+	</header>
+	<main>
+		<section class="grid">
+			<header>
+				<h1>Responsive Grid Homepage</h1>
+				<nav>
+					<ul>
+						<li><a href="#">Home</a></li>
+						<li><a href="#">About</a></li>
+						<li><a href="#">Contact</a></li>
+					</ul>
+				</nav>
+			</header>
+			<div class="item">
+				<h2>Section 1</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eget sodales nisi. Sed vel mi eu purus pulvinar euismod.</p>
+			</div>
+			<div class="item">
+				<h2>Section 2</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eget sodales nisi. Sed vel mi eu purus pulvinar euismod.</p>
+			</div>
+			<div class="item">
+				<h2>Section 3</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eget sodales nisi. Sed vel mi eu purus pulvinar euismod.</p>
+			</div>
+			<div class="item">
+				<h2>Section 4</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eget sodales nisi. Sed vel mi eu purus pulvinar euismod.</p>
+			</div>
+		</section>
+	</main>
+	<footer>
+		<p>Copyright &copy; 2023</p>
+	</footer>
+</body>
+</html>
